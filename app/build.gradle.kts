8a18d5ec0f3274ef38167d13ecf98ccfb73e6cb4
Lodesthard <133907854+Lodesthard@users.noreply.github.com>
2026-05-28 04:04:35 +0200

    chore: add Compose Navigation and Google Fonts dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -67,6 +67,8 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
+    implementation(libs.androidx.navigation.compose)
+    implementation(libs.androidx.compose.ui.text.google.fonts)
     implementation(libs.kotlinx.coroutines.android)
     implementation(libs.okhttp)
     implementation(libs.okhttp.logging)
